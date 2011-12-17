93a92d30f90fffef8b26078571722ef677adb473
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2011-12-17 19:22:44 +0100

    Remove unneeded imports

--- json_document/io.py
+++ json_document/io.py
@@ -16,13 +16,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with json-document.  If not, see <http://www.gnu.org/licenses/>.
 
-import copy
 import decimal
-import os
 
-from json_schema_validator.errors import SchemaError, ValidationError
-from json_schema_validator.schema  import Schema
-from json_schema_validator.validator import Validator
 import simplejson
 
 class DocumentIO(object):
